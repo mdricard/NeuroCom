6fd190570f4598de9fdbb6b3221c520f39ba00f8
Mark Ricard <ricard@uta.edu>
2025-04-06 18:09:41 -0500

    Added smoothing and plots

--- mct_reader.py
+++ mct_reader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from BiomechTools import zero_crossing, low_pass, critically_damped, residual_analysis
+from math import asin, degrees
 
 x = np.zeros(1250)
 lf = np.zeros(1250)
@@ -12,6 +13,8 @@ fp_cofx = np.zeros(1250)
 fp_cofy = np.zeros(1250)
 fp_cogx = np.zeros(1250)
 fp_cogy = np.zeros(1250)
+theta = np.zeros(1250)          # used to compute sway angle
+omega = np.zeros(1250)          # used to compute sway angular velocity
 def fix_string(messy_string, data_type):
     """
     Remove non-numeric characters from string
@@ -49,6 +52,7 @@ def read_neurocom_file_header(filename):
         ht_line = f.readline().rstrip()
         ht_str = ht_line.split(',')[1]
         ht = fix_string(ht_str, data_type=1)
+        cog_ht = 0.5527 * ht        # cog ht is 55.27 % of ht, see NeuroCom Appendix
         for i in range(4):
             f.readline()
         # ------   get test type as string   ---------------------------------
@@ -93,7 +97,7 @@ def read_neurocom_file_header(filename):
             fp_cogx[row] = fix_string(nmbr_str[8], data_type=1)
             fp_cogy[row] = fix_string(nmbr_str[9], data_type=1)
 
-    return subject, date_of_birth, ht, test_type, cond, sampling_rate, duration, n
+    return subject, date_of_birth, ht, cog_ht, test_type, cond, sampling_rate, duration, n
 
 def plot_cop():
     plt.plot(x[1:], fp_cofx[1:], label='fpcofx')
@@ -102,6 +106,15 @@ def plot_cop():
     plt.grid(True)
     plt.xlabel('Point Number (n)')
     plt.ylabel('Force (N)')
+    plt.show()
+
+def plot_cofy_theta():
+    plt.plot(x[1:], theta[1:], label='theta')
+    plt.plot(x[1:], smooth_cog_y[1:], label='Cog Y')
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel('Point Number (n)')
+    plt.ylabel('theta (d) Cog (cm)')
     plt.show()
 
 def plot_shear():
@@ -119,6 +132,19 @@ def plot_cog_and_shear():
     ax1.plot(x, smooth_cog_y, 'r-', label='Cog Y')
     ax2.plot(x, smooth_shear, 'k-', label='Shear')
     ax1.set_ylabel('COG (cm)')
+    ax2.set_ylabel('Shear Force (N)')
+    #fig.tight_layout()
+    plt.grid(True)
+    #plt.legend(['Cog X', 'Cog Y', 'Shear'])
+    ax1.legend([smooth_cog_y, smooth_shear], ['Cog Y', 'Shear'])
+    plt.show()
+
+def plot_theta_and_shear():
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(x, theta, 'r-', label='Theta')
+    ax2.plot(x, smooth_shear, 'k-', label='Shear')
+    ax1.set_ylabel('Theta COG (d)')
     ax2.set_ylabel('Shear Force (N)')
     #fig.tight_layout()
     plt.grid(True)
@@ -146,15 +172,20 @@ def plot_cof_cog_shear():
     plt.show()
 
 file_name = 'D:/Crystal_MCT/FD1528_PC-022_MCT_C5_T1_10162008_004621.txt'
-subject, date_of_birth, ht, test_type, cond_str, sampling_rate, duration, n = read_neurocom_file_header(filename=file_name)
+#file_name = 'D:/Crystal_MCT/FD1528_PC-022_SOT_C1_T1_10162008_003707.txt'
+subject, date_of_birth, ht, cog_ht, test_type, cond_str, sampling_rate, duration, n = read_neurocom_file_header(filename=file_name)
 #resid = residual_analysis(raw=sh, sampling_rate=sampling_rate, first_cutoff=2, last_cutoff=20, use_critical=False)
 smooth_shear = critically_damped(raw=sh, sampling_rate=sampling_rate, filter_cutoff=20)
-smooth_cog_x = low_pass(raw=fp_cogx, sampling_rate=sampling_rate, filter_cutoff=8)
-smooth_cog_y = low_pass(raw=fp_cogy, sampling_rate=sampling_rate, filter_cutoff=8)
-smooth_cof_x = low_pass(raw=fp_cofx, sampling_rate=sampling_rate, filter_cutoff=8)
-smooth_cof_y = low_pass(raw=fp_cofy, sampling_rate=sampling_rate, filter_cutoff=8)
-
+smooth_cog_x = low_pass(raw=fp_cogx, sampling_rate=sampling_rate, filter_cutoff=6)
+smooth_cog_y = low_pass(raw=fp_cogy, sampling_rate=sampling_rate, filter_cutoff=6)
+smooth_cof_x = low_pass(raw=fp_cofx, sampling_rate=sampling_rate, filter_cutoff=6)
+smooth_cof_y = low_pass(raw=fp_cofy, sampling_rate=sampling_rate, filter_cutoff=6)
+for i in range(len(smooth_cog_y)):
+    theta[i] = degrees(asin(smooth_cog_y[i]/cog_ht)) - 2.3          # See NeuroCom appendix for formula
+print('height: ', ht)
 plot_cof_cog_shear()
+plot_theta_and_shear()
+plot_cofy_theta()
 
 plot_cop()
 plot_cog_and_shear()
